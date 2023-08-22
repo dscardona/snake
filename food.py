0fd81534f06611226a647ea6b6be230d86cfce42
Diana Cardona <dscardona@gmail.com>
2023-08-22 16:52:57 -0400

    Detect collision, refresh food

--- food.py
+++ food.py
@@ -7,9 +7,13 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=10, stretch_wid=10)
+        self.shapesize(stretch_len=.5, stretch_wid=.5)
         self.color("blue")
         self.speed("fastest")
+        self.refresh()
+
+
+    def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
