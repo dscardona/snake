fad9df40bbc2a634a446b1502255e7b1ebf0472b
Diana Cardona <dscardona@gmail.com>
2023-08-22 16:32:16 -0400

    Minor edits: capitalization, spacing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("My snake game")
+screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
@@ -20,7 +20,6 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
 
