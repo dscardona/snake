dd7535377820eeda69b0b9b1df3ddae5d7aaf0ee
Diana Cardona <dscardona@gmail.com>
2023-08-22 17:31:57 -0400

    Create/implement scoreboard module and class

---
+++ scoreboard.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Arial', 20, 'normal')
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color("white")
+        self.score = 0
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def score_up(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
